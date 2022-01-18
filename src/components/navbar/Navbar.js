df5c762fbdf2f40e3ffef515277c663471b374d9
Débora Bucci <dbucci@protonmail.com>
2022-01-18 17:34:51 -0300

    create an array with all of the links content, return a <ul> wrapper with all of the content of the array returned as <li>

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,28 @@
 import React from "react";
 import "./Navbar.module.css";
 
-const Navbar = () => {};
+const Navbar = () => {
+  //// CONTENT ////
+  const linksList = [
+    { text: "Home", icon: "home", href: "#", key: "l1" },
+    { text: "About", icon: "address-card", href: "#", key: "l2" },
+    { text: "Portfolio", icon: "briefcase", href: "#", key: "l3" },
+    { text: "Contact", icon: "address-book", href: "#", key: "l4" },
+  ];
+  // -------------------------------------------------- //
+
+  return (
+    <ul>
+      {linksList.map(({ text, icon, href, key }) => {
+        return (
+          <li key={key}>
+            <i className={`fas fa-${icon}`}></i>
+            <a href={href}>{text}</a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default Navbar;
