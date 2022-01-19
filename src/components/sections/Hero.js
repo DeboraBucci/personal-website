6f0b7775168b956081b34099d2ae8af12be79b65
Débora Bucci <dbucci@protonmail.com>
2022-01-19 13:13:44 -0300

    create new component called 'Hero', which will be the main section that'll appear in the head of the page

---
+++ src/components/sections/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>
+        <p>HELLO !</p>
+        <h1>
+          I'm
+          <span>Débora Bucci</span>
+        </h1>
+        <p>a web developer</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/sections/Sections.js
+++ src/components/sections/Sections.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero";
 
 const Sections = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Sections;
