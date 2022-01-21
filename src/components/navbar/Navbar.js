48c84cee8d4ca9080452fb88f2933bed8bd0c130
Débora Bucci <dbucci@protonmail.com>
2022-01-21 14:28:41 -0300

    create and apply styles for the navbar, and also import and use some of the 'general-style' classes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,27 @@
 import React from "react";
-import styles from "./Navbar.module.css";
+import "./Navbar.css";
+import "../../general-styles.css";
 
 const Navbar = () => {
   //// CONTENT ////
   const linksList = [
     { text: "Home", icon: "home", href: "#", key: "l1" },
-    { text: "About", icon: "address-card", href: "#", key: "l2" },
+    { text: "Certifications", icon: "address-card", href: "#", key: "l2" },
     { text: "Portfolio", icon: "briefcase", href: "#", key: "l3" },
     { text: "Contact", icon: "address-book", href: "#", key: "l4" },
   ];
   // -------------------------------------------------- //
 
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles["list"]}>
+    <nav className="navbar flex h-centered v-centered">
+      <ul className="list flex">
         {linksList.map(({ text, icon, href, key }) => {
           return (
-            <li className={styles["link"]} key={key}>
-              <i className={`fas fa-${icon}`}></i>
+            <li
+              className="link flex h-centered c-white g-10px pointer"
+              key={key}
+            >
+              <i className={`fas fa-${icon} inline-blk `}></i>
               <a href={href}>{text}</a>
             </li>
           );
