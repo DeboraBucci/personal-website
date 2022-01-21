5009964040480bd5966887b08fda661a224c91df
Débora Bucci <dbucci@protonmail.com>
2022-01-21 17:43:05 -0300

    modify or delete previously created classes. Also, import the components called 'Projects', 'Header' and 'LinkButton' and returned them wrapped inside a <section> element, and also import the stylesheet called 'general-stylesheet'.

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Projects from "./Projects";
+import Header from "./Header";
+import LinkButton from "./LinkButton";
 import "./Portfolio.css";
+import "../../general-styles.css";
 
 const Portfolio = () => {
-  return <section></section>;
+  return (
+    <section>
+      <Header title="Here are my projects" subtitle="projects" />
+      <Projects />
+      <LinkButton
+        href="https://github.com/DeboraBucci?tab=repositories"
+        text="See all my projects"
+        icon={<i className="fas fa-angle-right"></i>}
+        classes="all-projects-btn flex centered g-10px c-white"
+      />
+    </section>
+  );
 };
 
 export default Portfolio;
