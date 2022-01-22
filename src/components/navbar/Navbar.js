c5abfaae2192e65ba137199429ca0ee11eefaa5a
Débora Bucci <dbucci@protonmail.com>
2022-01-22 12:13:59 -0300

    add rest of the 'href' links of the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,9 +6,14 @@ const Navbar = () => {
   //// CONTENT ////
   const linksList = [
     { text: "Home", icon: "home", href: "#home", key: "l1" },
-    { text: "Certifications", icon: "award", href: "#", key: "l2" },
-    { text: "Portfolio", icon: "briefcase", href: "#", key: "l3" },
-    { text: "Contact", icon: "paper-plane", href: "#", key: "l4" },
+    {
+      text: "Certifications",
+      icon: "award",
+      href: "#cetifications",
+      key: "l2",
+    },
+    { text: "Portfolio", icon: "briefcase", href: "#portfolio", key: "l3" },
+    { text: "Contact", icon: "paper-plane", href: "#contact", key: "l4" },
   ];
   // -------------------------------------------------- //
 
