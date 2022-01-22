d013357eb6d8caafb316f45e95277c133ec6e539
Débora Bucci <dbucci@protonmail.com>
2022-01-22 12:24:41 -0300

    create a new component called 'CoursesCarousel' and use it to display a carousel with the help of 'react-botstrap', and modify it so it maps over the images that you want to be displayed, and renders them wrapped in the <Carousel.Item> component, with a <div>inside, wrapping an <img> element, and another <div> as a sibling for creating space, and with all the elements having their own personal styles and disposition

---
+++ src/components/carousel/CoursesCarousel.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import Course01 from "../../imgs/certifications/course-01.jpg";
+import Course02 from "../../imgs/certifications/course-02.jpg";
+import Course03 from "../../imgs/certifications/course-03.jpg";
+import Course04 from "../../imgs/certifications/course-04.jpg";
+import Course05 from "../../imgs/certifications/course-05.png";
+
+import "./CoursesCarousel.css";
+
+const CoursesCarousel = () => {
+  const coursesImages = [
+    {
+      image: Course01,
+      alt: "First",
+      interval: 1000,
+    },
+    {
+      image: Course02,
+      alt: "Second",
+    },
+    {
+      image: Course03,
+      alt: "Third",
+    },
+    {
+      image: Course04,
+      alt: "Fourth",
+    },
+    {
+      image: Course05,
+      alt: "Fifth",
+    },
+  ];
+
+  return (
+    <div className="carousel-container mrg-auto">
+      <Carousel touch={true}>
+        {coursesImages.map(({ image, alt }) => {
+          return (
+            <Carousel.Item interval={3000}>
+              <div className="img-container mrg-auto overflow-hdn">
+                <img
+                  className="d-block w-100"
+                  src={image}
+                  alt={`${alt} slide`}
+                />
+                <div className="empty-space"></div>
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
+
+export default CoursesCarousel;
