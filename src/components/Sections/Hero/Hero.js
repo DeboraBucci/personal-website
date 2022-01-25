dbfc79684bbf6cdb1258685ce0dd18ce9ac0a00f
Debora Bucci <dbucci@protonmail.com>
2022-01-25 14:23:11 -0300

    Delete all the content about me, and change the stylesheet of the 'Hero' section to be a module

--- src/components/Sections/Hero/Hero.js
+++ src/components/Sections/Hero/Hero.js
@@ -1,36 +1,21 @@
 import React from "react";
-import Typing from "../../Typing/Typing";
 import Introduction from "./Introduction";
 import Button from "../../Button/Button";
-import "./Hero.css";
+import classes from "./Hero.module.css";
 import "../../../general-styles.css";
 
 const Hero = () => {
   return (
     <section id={"home"} className={"flex column centered "}>
-      <div className="introduction-box">
-        <Introduction fullName="Débora Bucci" />
-        <div className={"profession flex h-centered g-10px"}>
-          <span>a</span>
-          <span className={"typing inline-blk"}>
-            <Typing
-              words={[
-                "Frontend Developer",
-                "Web Designer",
-                "Forever Student",
-                "Devote Christian",
-              ]}
-              cursorType={"|"}
-            />
-          </span>
-        </div>
-      </div>
-      <div className="actions flex centered">
+      <Introduction />
+      <div className={classes.actions}>
         <Button classes="cv-btn">
-          Download CV <i className="fas fa-file-download"></i>
+          Download CV
+          <i className="fas fa-file-download"></i>
         </Button>
         <Button>
-          Contact Me <i className="fas fa-angle-down"></i>
+          Contact Me
+          <i className="fas fa-angle-down"></i>
         </Button>
       </div>
     </section>
