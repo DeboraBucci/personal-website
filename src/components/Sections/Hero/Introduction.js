ae8cd2ed8e7703e3c0e46eff91e08a1af2066fe5
Debora Bucci <dbucci@protonmail.com>
2022-01-25 14:18:56 -0300

    put all the information about me inside the <Introduction> component, and create a stylesheet module for the elements inside

--- src/components/Sections/Hero/Introduction.js
+++ src/components/Sections/Hero/Introduction.js
@@ -1,15 +1,33 @@
 import React from "react";
+import Typing from "../../Typing/Typing";
+import classes from "./Introduction.module.css";
 
-const Introduction = ({ greeting = "HELLO !", fullName = "Anonymous" }) => {
+const Introduction = () => {
   return (
-    <div className="introduction--container">
-      <p className={"flex h-centered g-10px"}>{greeting}</p>
-      <h1>
-        <span>I'm</span>
-        <div className={"underline--magical flex v-centered"}>
-          <div>{fullName}</div>
-        </div>
-      </h1>
+    <div className="container flex column centered">
+      <div>
+        <p className={classes.greeting}>Hello!</p>
+
+        <h1 className={classes.title}>
+          <span>I'm</span>
+          <div className={"underline--magical"}>Débora Bucci</div>
+        </h1>
+      </div>
+
+      <p className={classes.professions}>
+        <span>a</span>
+        <span className={classes.typing}>
+          <Typing
+            words={[
+              "Frontend Developer",
+              "Web Designer",
+              "Forever Student",
+              "Devote Christian",
+            ]}
+            cursorType={"|"}
+          />
+        </span>
+      </p>
     </div>
   );
 };
