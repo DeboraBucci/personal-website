7e775f5e27a0ee8358ee3773de0d5dca4bded2e3
Debora Bucci <dbucci@protonmail.com>
2022-01-25 14:51:11 -0300

    display the content in the middle of the page, and the <p> elements closer to everything

--- src/components/Sections/Hero/Introduction.js
+++ src/components/Sections/Hero/Introduction.js
@@ -5,29 +5,29 @@ import classes from "./Introduction.module.css";
 const Introduction = () => {
   return (
     <Fragment>
-      <div>
+      <div className={classes.content}>
         <p className={classes.greeting}>Hello!</p>
 
         <h1 className={classes.title}>
           <span>I'm</span>
           <div className={"underline--magical"}>Débora Bucci</div>
         </h1>
-      </div>
 
-      <p className={classes.professions}>
-        <span>a</span>
-        <span className={classes.typing}>
-          <Typing
-            words={[
-              "Frontend Developer",
-              "Web Designer",
-              "Forever Student",
-              "Devote Christian",
-            ]}
-            cursorType={"|"}
-          />
-        </span>
-      </p>
+        <p className={classes.professions}>
+          <span>a</span>
+          <span className={classes.typing}>
+            <Typing
+              words={[
+                "Frontend Developer",
+                "Web Designer",
+                "Forever Student",
+                "Devote Christian",
+              ]}
+              cursorType={"|"}
+            />
+          </span>
+        </p>
+      </div>
     </Fragment>
   );
 };
