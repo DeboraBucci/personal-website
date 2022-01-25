457cd6bee8d7b95a40679048ead8eb17913a6c8d
Debora Bucci <dbucci@protonmail.com>
2022-01-25 15:57:25 -0300

    change the stylesheet file of the 'Portfolio' to a module, and apply all its classes

--- src/components/Sections/Portfolio.js
+++ src/components/Sections/Portfolio.js
@@ -2,23 +2,25 @@ import React, { Fragment } from "react";
 import Projects from "./Projects";
 import Header from "../Header/Header";
 import Button from "../Button/Button";
-import "./Portfolio.css";
+import classes from "./Portfolio.module.css";
 
 const Portfolio = () => {
   return (
     <Fragment>
       <Header title="Here are my projects" subtitle="projects" />
       <Projects />
-      <a
-        target="_blank"
-        rel="noopener"
-        href="https://github.com/DeboraBucci?tab=repositories"
-      >
-        <Button>
-          See all my projects
-          <i className="fas fa-angle-right"></i>
-        </Button>
-      </a>
+      <div className={classes.actions}>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/DeboraBucci?tab=repositories"
+        >
+          <Button classes={classes["all-projects-btn"]}>
+            See all my projects
+            <i className="fas fa-angle-right"></i>
+          </Button>
+        </a>
+      </div>
     </Fragment>
   );
 };
