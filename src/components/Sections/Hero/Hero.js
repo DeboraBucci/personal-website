0490d238d60015ac43bd890c95330588be645cb3
Debora Bucci <dbucci@protonmail.com>
2022-01-25 13:08:35 -0300

    add the 'column' and 'centered' class, and remove 'h-centered' and 'v-centered'

--- src/components/Sections/Hero/Hero.js
+++ src/components/Sections/Hero/Hero.js
@@ -6,7 +6,7 @@ import "../../../general-styles.css";
 
 const Hero = () => {
   return (
-    <section id={"home"} className={"flex h-centered v-centered "}>
+    <section id={"home"} className={"flex column centered "}>
       <div>
         <Introduction fullName="Débora Bucci" />
         <div className={"profession flex h-centered g-10px"}>
