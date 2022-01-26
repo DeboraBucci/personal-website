46b0d6fcc1f2d7bd77ef085f82fb5aecedf41158
Debora Bucci <dbucci@protonmail.com>
2022-01-26 13:05:30 -0300

    add the underline for the header inside the 'Header.module.css' file

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 
 const Header = ({ title = "TITLE", subtitle = "SECTION" }) => {
   return (
-    <header>
+    <header className={styles.header}>
       <span className={styles.subheading}>#{subtitle}</span>
       <h2 className={styles.heading}>{title}</h2>
     </header>
