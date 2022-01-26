a63c93d19a813cd5a1e7711a0e92d25b6926a870
Debora Bucci <dbucci@protonmail.com>
2022-01-26 11:08:38 -0300

    create a file called 'Data.js' to store all the data from the components and export it all. Store the data from the 'Navbar' component

---
+++ src/Data.js
@@ -0,0 +1,13 @@
+const linksList = [
+  { text: "Home", icon: "home", href: "#home", key: "l1" },
+  {
+    text: "Certifications",
+    icon: "award",
+    href: "#certifications",
+    key: "l2",
+  },
+  { text: "Portfolio", icon: "briefcase", href: "#portfolio", key: "l3" },
+  { text: "Contact", icon: "paper-plane", href: "#contact", key: "l4" },
+];
+
+export { linksList };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,9 @@
 import React from "react";
 import classes from "./Navbar.module.css";
 import Icon from "../UI/Icon";
+import { linksList } from "../../Data";
 
 const Navbar = () => {
-  const linksList = [
-    { text: "Home", icon: "home", href: "#home", key: "l1" },
-    {
-      text: "Certifications",
-      icon: "award",
-      href: "#certifications",
-      key: "l2",
-    },
-    { text: "Portfolio", icon: "briefcase", href: "#portfolio", key: "l3" },
-    { text: "Contact", icon: "paper-plane", href: "#contact", key: "l4" },
-  ];
-
   return (
     <nav className={`${classes.navbar} flex centered`}>
       <ul className={`${classes.list} flex`}>
