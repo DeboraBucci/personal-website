dec20bb85069e752ff153af9d3b4cf6806aaa60c
Debora Bucci <dbucci@protonmail.com>
2022-01-26 11:12:16 -0300

    place all the data from the 'Projects' in the 'Data.js' file, and import it so it can be used by the component again

--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,10 @@
+import pigGame from "./imgs/projects/pig-game-project.png";
+import googleHomepage from "./imgs/projects/google-homepage-project.png";
+import tributePage from "./imgs/projects/tribute-page-project.png";
+import surveyForm from "./imgs/projects/survey-form-project.png";
+import technicalDocumentation from "./imgs/projects/technical-documentation-project.png";
+import productLanding from "./imgs/projects/product-landing-project.png";
+
 const linksList = [
   { text: "Home", icon: "home", href: "#home", key: "l1" },
   {
@@ -10,4 +17,43 @@ const linksList = [
   { text: "Contact", icon: "paper-plane", href: "#contact", key: "l4" },
 ];
 
-export { linksList };
+const projects = [
+  {
+    title: "Tribute Page",
+    link: "tribute-page",
+    image: tributePage,
+    key: "project--01",
+  },
+  {
+    title: "Product Landing Page",
+    link: "product-landing-page",
+    image: productLanding,
+    key: "project--02",
+  },
+  {
+    title: "Pig Game",
+    link: "pig-game",
+    image: pigGame,
+    key: "project--03",
+  },
+  {
+    title: "Google's Homepage",
+    link: "google-homepage",
+    image: googleHomepage,
+    key: "project--04",
+  },
+  {
+    title: "Technical Documentation Page",
+    link: "technical-documentation-page",
+    image: technicalDocumentation,
+    key: "project--05",
+  },
+  {
+    title: "Survey Form",
+    link: "survey-form",
+    image: surveyForm,
+    key: "project--06",
+  },
+];
+
+export { linksList, projects };
--- src/components/Sections/Projects.js
+++ src/components/Sections/Projects.js
@@ -1,53 +1,8 @@
 import React from "react";
-import pigGame from "../../imgs/projects/pig-game-project.png";
-import googleHomepage from "../../imgs/projects/google-homepage-project.png";
-import tributePage from "../../imgs/projects/tribute-page-project.png";
-import surveyForm from "../../imgs/projects/survey-form-project.png";
-import technicalDocumentation from "../../imgs/projects/technical-documentation-project.png";
-import productLanding from "../../imgs/projects/product-landing-project.png";
-
 import classes from "./Projects.module.css";
+import { projects } from "../../Data";
 
 const Projects = () => {
-  const projects = [
-    {
-      title: "Tribute Page",
-      link: "tribute-page",
-      image: tributePage,
-      key: "project--01",
-    },
-    {
-      title: "Product Landing Page",
-      link: "product-landing-page",
-      image: productLanding,
-      key: "project--02",
-    },
-    {
-      title: "Pig Game",
-      link: "pig-game",
-      image: pigGame,
-      key: "project--03",
-    },
-    {
-      title: "Google's Homepage",
-      link: "google-homepage",
-      image: googleHomepage,
-      key: "project--04",
-    },
-    {
-      title: "Technical Documentation Page",
-      link: "technical-documentation-page",
-      image: technicalDocumentation,
-      key: "project--05",
-    },
-    {
-      title: "Survey Form",
-      link: "survey-form",
-      image: surveyForm,
-      key: "project--06",
-    },
-  ];
-
   const changeElementsColor = (arr, color) => {
     for (let i = 0; i < arr.length; i++) {
       arr[i].style.color = color;
