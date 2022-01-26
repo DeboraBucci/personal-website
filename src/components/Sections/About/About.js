bd7451a457367ec71b4fcca7910c718a63eb3cb7
Debora Bucci <dbucci@protonmail.com>
2022-01-26 13:07:33 -0300

    change the <Fragment> component for a <div> instead

--- src/components/Sections/About/About.js
+++ src/components/Sections/About/About.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { traits } from "../../../Data";
 import Header from "../../UI/Header";
+import classes from "./About.module.css";
 
 const About = () => {
   return (
-    <Fragment>
+    <div>
       <Header title="Get to know me more" subtitle="about me" />
       <div>
         <p>
@@ -26,7 +27,7 @@ const About = () => {
           ))}
         </ul>
       </div>
-    </Fragment>
+    </div>
   );
 };
 
