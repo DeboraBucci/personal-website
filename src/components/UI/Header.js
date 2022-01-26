83311947327ca6d14a4ab3e594aaffc3f1d8ee2e
Debora Bucci <dbucci@protonmail.com>
2022-01-26 13:23:30 -0300

    add a new <div> with a class of 'container', and modify the padding around it

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -4,8 +4,10 @@ import classes from "./Header.module.css";
 const Header = ({ title = "TITLE", subtitle = "SECTION" }) => {
   return (
     <header className={classes.header}>
-      <span className={classes.subheading}>#{subtitle}</span>
-      <h2 className={classes.heading}>{title}</h2>
+      <div className={classes.container}>
+        <span className={classes.subheading}>#{subtitle}</span>
+        <h2 className={classes.heading}>{title}</h2>
+      </div>
     </header>
   );
 };
