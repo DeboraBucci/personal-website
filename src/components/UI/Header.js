63b99f4780280c232007e20a050be3f97904e560
Debora Bucci <dbucci@protonmail.com>
2022-01-28 23:18:59 -0300

    delete the <div> wrapping the heading and subheading, and at its style to the <header>. add media queries to make the 'Header' component more responsive

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -4,10 +4,8 @@ import classes from "./Header.module.css";
 const Header = ({ title = "TITLE", subtitle = "SECTION" }) => {
   return (
     <header className={classes.header}>
-      <div className={classes.container}>
-        <span className={classes.subheading}>#{subtitle}</span>
-        <h2 className={classes.heading}>{title}</h2>
-      </div>
+      <span className={classes.subheading}>#{subtitle}</span>
+      <h2 className={classes.heading}>{title}</h2>
     </header>
   );
 };
