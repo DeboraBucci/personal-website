8f28c5e5c9bae035a0801ec3c75e885b2e6ffe51
Debora Bucci <dbucci@protonmail.com>
2022-01-29 00:15:19 -0300

    create a new component called 'Contact', which will be a section with a welcoming text, and three links to: my LinkedIn, GitHub, and e-mail

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,48 @@
+import React from "react";
+import classes from "./Contact.module.css";
+
+const Contact = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.heading}>
+        <p className={classes.subheading}>Let's talk!</p>
+        <h2>Contact info</h2>
+        <p>A penny for your thoughts!</p>
+      </div>
+      <ul className={classes.list}>
+        <li className={classes["link-box"]}>
+          <a
+            className={classes.link}
+            href="https://www.linkedin.com/in/d%C3%A9bora-bucci-13a07a212/"
+            target={"_blank"}
+          >
+            <i class="fab fa-linkedin"></i>
+            LinkedIn
+          </a>
+        </li>
+        <li className={classes["link-box"]}>
+          <a
+            className={classes.link}
+            href="https://github.com/DeboraBucci"
+            target={"_blank"}
+          >
+            <i class="fab fa-github-alt"></i>
+            GitHub
+          </a>
+        </li>
+        <li className={classes["link-box"]}>
+          <a
+            className={classes.link}
+            href="mailto:dbucci@protonmail.com"
+            target={"_blank"}
+          >
+            <i class="fas fa-at"></i>
+            send me an e-mail
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
