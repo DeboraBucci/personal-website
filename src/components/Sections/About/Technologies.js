9a3d18c6ce06cd082f922c6e87670b8ec87ddb16
Debora Bucci <dbucci@protonmail.com>
2022-01-29 17:55:27 -0300

    remove the state and add the same functionality (when hovering over an icon, the others become blurred and grey) with only css selectors and styles

--- src/components/Sections/About/Technologies.js
+++ src/components/Sections/About/Technologies.js
@@ -1,29 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { technologies } from "../../../Data";
 import classes from "./Technologies.module.css";
 
 const Technologies = () => {
-  const [isOver, setIsOver] = useState(false);
-
-  const filterOn = () => {
-    setIsOver(true);
-  };
-
-  const filterOff = () => {
-    setIsOver(false);
-  };
   return (
     <ul className={classes["icons-container"]}>
       {technologies.map(({ text, icon }) => (
-        <li
-          className={classes["list-el"]}
-          key={Math.random().toString()}
-          onMouseEnter={filterOn}
-          onMouseLeave={filterOff}
-        >
-          <div
-            className={isOver ? classes["filter-on"] : classes["filter-off"]}
-          >
+        <li className={classes["list-el"]} key={Math.random().toString()}>
+          <div>
             <img src={icon} style={{ width: "50px", height: "50px" }} />
           </div>
           <span>{text}</span>
