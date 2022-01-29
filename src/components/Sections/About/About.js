62836e5cd72ed78dcb8c835839537ba7174dd930
Debora Bucci <dbucci@protonmail.com>
2022-01-28 22:54:29 -0300

    clean code, organize classes, change classes' names to more intuitive ones, delete extra or repeated code, etc.

--- src/components/Sections/About/About.js
+++ src/components/Sections/About/About.js
@@ -18,44 +18,52 @@ const About = () => {
     <Fragment>
       <Header title="Get to know me more" subtitle="about me" />
       <main className={classes["info-container"]}>
-        <p>
-          I'm a 21 years-old web-developer who lives in Argentina, in the city
-          of Rosario. Ever since I started to design and code websites in March
-          of 2021, I absolutely fell in love with it.
-        </p>
+        <div className={classes.box}>
+          <p>
+            I'm a 21 years-old web-developer who lives in Argentina, in the city
+            of Rosario. Ever since I started to design and code websites in
+            March of 2021, I absolutely fell in love with it.
+          </p>
+        </div>
 
-        <ul className={classes.list}>
-          <h3 className={classes.title}>
+        <div className={classes.box}>
+          <h3 className={classes["list-title"]}>
             <span className={classes.text}>SOME OF MY TRAITS</span>
           </h3>
-          {traits.map(({ title, content }) => (
-            <li key={Math.random()}>
-              <span className={classes["list-title"]}>{title}</span>
-              <span> — {content}</span>
-            </li>
-          ))}
-        </ul>
+          <ul className={classes.list}>
+            {traits.map(({ title, content }) => (
+              <li key={Math.random()}>
+                <span className={classes["traits-heading"]}>{title}</span>
+                <span> — {content}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-        <ul className={classes.list}>
-          <h3 className={classes.title}>
+        <div className={classes.box}>
+          <h3 className={classes["list-title"]}>
             <span className={classes.text}>Technologies I Use</span>
           </h3>
-          <div className={classes["icon-container"]}>
+          <ul className={classes["icons-container"]}>
             {technologies.map(({ text, icon }) => (
               <li
+                className={classes["list-el"]}
                 key={Math.random().toString()}
                 onMouseEnter={filterOn}
                 onMouseLeave={filterOff}
               >
-                <div className={isOver ? classes.grey : ""}>
-                  <img src={icon} style={{ width: "50px" }} />
+                <div
+                  className={
+                    isOver ? classes["filter-on"] : classes["filter-off"]
+                  }
+                >
+                  <img src={icon} style={{ width: "50px", height: "50px" }} />
                 </div>
                 <span>{text}</span>
               </li>
             ))}
-          </div>
-        </ul>
-
+          </ul>
+        </div>
         <div className={classes["last-line"]}></div>
       </main>
     </Fragment>
