a001d7f512338df962fd49562a0e3c3ba53a1b19
Debora Bucci <dbucci@protonmail.com>
2022-01-30 17:25:09 -0300

    remove the <div> wrapping the <Link> component and add its styles to the <Link> itself

--- src/components/Sections/Portfolio/Portfolio.js
+++ src/components/Sections/Portfolio/Portfolio.js
@@ -15,14 +15,15 @@ const Portfolio = () => {
         className={"portfolio-heading"}
       />
       <Projects />
-      <div className={classes.actions}>
-        <Link href="https://github.com/DeboraBucci?tab=repositories">
-          <Button className={classes["all-projects-btn"]}>
-            See all my projects
-            <Icon icon={"fas fa-angle-right"} />
-          </Button>
-        </Link>
-      </div>
+      <Link
+        className={classes.link}
+        href="https://github.com/DeboraBucci?tab=repositories"
+      >
+        <Button className={classes["all-projects-btn"]}>
+          See all my projects
+          <Icon icon={"fas fa-angle-right"} />
+        </Button>
+      </Link>
     </Fragment>
   );
 };
