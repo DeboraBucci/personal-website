777f3ec1c67855dfe7ba83926c3f33e783fccb23
Debora Bucci <dbucci@protonmail.com>
2022-03-24 16:30:40 -0300

    merge all the Hero section content into one file, and delete both stylesheets. Remove the classes import and use the BEM naming strategy to name the new classes

--- src/components/Sections/Hero/Hero.js
+++ src/components/Sections/Hero/Hero.js
@@ -1,23 +1,30 @@
 import React from "react";
-import Introduction from "./Introduction";
-import Button from "../../UI/Button";
-import classes from "./Hero.module.css";
+import Typing from "../../UI/Typing";
 
 const Hero = () => {
   return (
-    <div className={classes.content}>
-      <Introduction />
-      <div className={classes.actions}>
-        {/* <Button classes={classes["cv-btn"]}>
-          Download CV
-          <i className="fas fa-file-download"></i>
-        </Button>
-        <Button>
-          Contact Me
-          <i className="fas fa-angle-down"></i>
-        </Button> */}
+    <section className="hero" id="home">
+      <div className="hero__text">
+        <p className="hero__text--greeting">Hello!</p>
+
+        <h1 className="heading-primary">
+          <span className="heading-primary__first-part">I'm</span>
+          <span className="heading-primary__second-part underline--magical">
+            Débora Bucci
+          </span>
+        </h1>
+
+        <p className="hero__text--professions">
+          <span>a</span>
+          <span className="hero__text--professions-typing">
+            <Typing
+              words={["Frontend Developer", "Web Designer", "Forever Student"]}
+              cursorType={"|"}
+            />
+          </span>
+        </p>
       </div>
-    </div>
+    </section>
   );
 };
 
