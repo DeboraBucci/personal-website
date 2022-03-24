6d2b49415c9277889afa92de1083c78fb1ad7d07
Debora Bucci <dbucci@protonmail.com>
2022-03-24 20:54:34 -0300

    create a new component called 'Vignettes' with the paragraphs of the 'About' section, with some classes that follows the BEM naming convention

---
+++ src/components/Sections/About/Vignettes.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Vignettes = () => {
+  return (
+    <div className="vignettes u-margin-bottom-medium">
+      <div className="vignettes__line"></div>
+
+      <p className="vignettes--1">
+        I'm a Frontend Developer who specializes in React, and is passionate
+        about coding and improving. I'm not afraid of making mistakes if that
+        means I can learn from them.
+      </p>
+      <p className="vignettes--2">
+        I started coding at the beginning of 2021, and ever since I've come to
+        learn many aspects of this career, growing more resilient with the
+        passing time.
+      </p>
+    </div>
+  );
+};
+
+export default Vignettes;
