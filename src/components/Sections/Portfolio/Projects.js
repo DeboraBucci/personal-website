7a092c9a159604a99209c10a3e4ee91569a5bc95
Debora Bucci <dbucci@protonmail.com>
2022-03-24 14:11:46 -0300

    change the styles of the displayed projects

--- src/components/Sections/Portfolio/Projects.js
+++ src/components/Sections/Portfolio/Projects.js
@@ -3,61 +3,65 @@ import classes from "./Projects.module.css";
 import { projects } from "../../../Data";
 
 const Projects = () => {
-  const changeElementsColor = (arr, color) => {
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].style.color = color;
-    }
-  };
-
-  const selectSpanTitle = (e) => {
-    return e.target
-      .closest(".project__container")
-      .querySelectorAll(".span-title");
-  };
-
-  const showColorHandler = (e) => {
-    changeElementsColor(selectSpanTitle(e), "#ff7300");
-  };
-
-  const hideColorHandler = (e) => {
-    changeElementsColor(selectSpanTitle(e), "transparent");
-  };
-
   return (
-    <main className={classes.main}>
-      {projects.map(({ title, link, image, key }) => {
+    <div className={classes.projects}>
+      {projects.map(({ title, link, num, info }) => {
         return (
-          <div
-            key={key}
-            className={`project__container ${classes.project}`}
-            onMouseEnter={showColorHandler}
-            onMouseLeave={hideColorHandler}
-          >
-            <a
-              href={`https://deborabucci.github.io/${link}/`}
-              target={"_blank"}
-              rel="noopener"
+          <div className={classes.card} key={`project-${num}`}>
+            <div
+              className={`${classes["card__side"]} ${classes["card__side--front"]}`}
             >
-              <div className={classes["img-container"]}>
-                <img
-                  alt={`${title} project`}
-                  className={classes.img}
-                  width={400}
-                  height={400}
-                  src={image}
-                />
+              <div
+                className={`${classes["card__picture"]} ${
+                  classes[`card__picture-${num}`]
+                }`}
+              >
+                &nbsp;
               </div>
+              <h3 className={classes["card__heading"]}>{title}</h3>
 
-              <h3 className={classes.title}>
-                <span className="span-title">&lt;</span>
-                {title}
-                <span className="span-title">/&gt;</span>
-              </h3>
-            </a>
+              <div className={classes["card__details"]}>
+                <ul className={classes["card__details-list"]}>
+                  {info.map((item) => (
+                    <li>
+                      <span className={classes["item__first-part"]}>
+                        {item.title}:
+                      </span>
+                      <span className={classes["item__second-part"]}>
+                        {item.text}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div
+              className={`${classes["card__side"]} ${
+                classes["card__side--back"]
+              } ${classes[`card__side--back-${num}`]}`}
+            >
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <h3>
+                    <span className="card__heading-span card__heading-span--2">
+                      {title}
+                    </span>
+                  </h3>
+                </div>
+                <a
+                  href={`https://deborabucci.github.io/${link}/`}
+                  target={"_blank"}
+                  rel="noopener"
+                  className="btn btn--white"
+                >
+                  Visit Project
+                </a>
+              </div>
+            </div>
           </div>
         );
       })}
-    </main>
+    </div>
   );
 };
 
