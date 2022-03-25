12223f67c1a976fcf1dae62a188e9259486a667a
Debora Bucci <dbucci@protonmail.com>
2022-03-24 21:05:22 -0300

    delete the Header's stylesheet, and add new classes to the component following the BEM naming convention

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,15 +1,10 @@
 import React from "react";
-import classes from "./Header.module.css";
 
-const Header = ({
-  title = "TITLE",
-  subtitle = "SECTION",
-  className = "default",
-}) => {
+const Header = ({ title, subtitle }) => {
   return (
-    <header className={`${classes.header} ${classes[className]}`}>
-      <span className={classes.subheading}>#{subtitle}</span>
-      <h2 className={classes.heading}>{title}</h2>
+    <header className="heading">
+      <span className="heading__subtitle">#{subtitle}</span>
+      <h2 className="heading-secondary">{title}</h2>
     </header>
   );
 };
