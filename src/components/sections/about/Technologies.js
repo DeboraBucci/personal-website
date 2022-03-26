d5b5efcb746e0a9c97a4e44863cfbd6536190a46
Debora Bucci <dbucci@protonmail.com>
2022-03-26 13:06:19 -0300

    create the hovering effect over the icons to happen with css selections, and remove all previous code respecting that effect from the 'Technologies' component

--- src/components/sections/about/Technologies.js
+++ src/components/sections/about/Technologies.js
@@ -1,37 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { technologies } from "../../../Data";
 
 const Technologies = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const filterOn = () => {
-    setIsHovered(true);
-  };
-
-  const filterOff = () => {
-    setIsHovered(false);
-  };
-
   return (
     <div className="technologies u-center-text">
       <h3 className="heading-tertiary u-margin-bottom-medium ">
         Technologies I Use
       </h3>
 
-      <ul className="technologies__list" onMouseLeave={filterOff}>
+      <ul className="technologies__list">
         {technologies.map(({ text, icon }) => (
-          <li
-            className={`technologies__item ${
-              isHovered && "technologies__filter-on"
-            }`}
-            key={Math.random().toString()}
-            onMouseEnter={filterOn}
-            onMouseLeave={filterOff}
-          >
-            <div
-              className="technologies__item--img-container"
-              onMouseLeave={filterOff}
-            >
+          <li className="technologies__item" key={Math.random().toString()}>
+            <div className="technologies__item--img-container">
               <img
                 alt={`${icon} icon`}
                 src={icon}
