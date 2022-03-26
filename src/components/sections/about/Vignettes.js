5cd3d7c8a5e342c44de22d42bc2f594eeb568698
Debora Bucci <dbucci@protonmail.com>
2022-03-26 13:03:04 -0300

    update Vignettes' text

--- src/components/sections/about/Vignettes.js
+++ src/components/sections/about/Vignettes.js
@@ -7,11 +7,11 @@ const Vignettes = () => {
 
       <p className="vignettes--1">
         I'm a Frontend Developer who specializes in React, and is passionate
-        about coding and improving. I'm not afraid of making mistakes if that
-        means I can learn from them.
+        about learning, coding and developing. I'm not afraid of making mistakes
+        if that means I can learn from them.
       </p>
       <p className="vignettes--2">
-        I started coding at the beginning of 2021, and ever since I've come to
+        I started coding at the beginning of 2021, and ever since, I've come to
         learn many aspects of this career, growing more resilient with the
         passing time.
       </p>
