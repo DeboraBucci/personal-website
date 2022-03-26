b8c85cc85587e3263118ed681428d6ddce1bc457
Debora Bucci <dbucci@protonmail.com>
2022-03-26 13:21:27 -0300

    delay the Typing animation by 1.5 seconds

--- src/components/UI/Typing.js
+++ src/components/UI/Typing.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
 
 const Typing = ({ words, cursorType }) => {
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsPageLoaded(true);
+    }, 1500);
+  }, []);
+
   return (
-    <Typewriter
-      words={words}
-      loop={false}
-      cursor
-      cursorStyle={cursorType}
-      typeSpeed={70}
-      deleteSpeed={50}
-      delaySpeed={2000}
-    />
+    <>
+      {isPageLoaded && (
+        <Typewriter
+          words={words}
+          loop={false}
+          cursor
+          cursorStyle={cursorType}
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={2000}
+        />
+      )}
+    </>
   );
 };
 
