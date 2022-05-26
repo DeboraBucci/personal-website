6229366481486021fee8ed8a1c56fb04cdb41b5b
Debora Bucci <dbucci@protonmail.com>
2022-05-26 15:47:22 -0300

    add a header and two certificate images to the 'Certifications' component, and some basic styles

--- src/components/sections/certifications/Certifications.js
+++ src/components/sections/certifications/Certifications.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { certificationImages } from "../../../Data";
+import Header from "../../UI/Header";
+import Link from "../../UI/Link";
 
 const Certifications = () => {
-  return <div>Certifications</div>;
+  return (
+    <section className="certifications" id="certifications">
+      <Header title="These are my certificates" subtitle="certifications" />
+
+      <div className="certifications__container">
+        {certificationImages.map((certificate) => (
+          <div className="certifications__image-box">
+            <Link href={certificate.href}>
+              <img
+                src={certificate.image}
+                alt={`${certificate.alt} certificate`}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Certifications;
