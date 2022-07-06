5bd6e6347816dc073e8557526c3397c4ad346a75
Debora Bucci <dbucci@protonmail.com>
2022-07-06 14:51:21 -0300

    make navbar disappear in small screens, and add a button to make it appear again if needed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { linksList } from "../../Data";
 
 const Navbar = () => {
+  const [isNavbarOpened, setIsNavbarOpened] = useState(false);
+
+  const handleNavState = () =>
+    isNavbarOpened ? setIsNavbarOpened(false) : setIsNavbarOpened(true);
+
   return (
-    <nav className="navigation">
-      <ul className="navigation__list">
-        {linksList.map(({ text, icon, href, key }) => (
-          <li className="navigation__item" key={key}>
-            <a className="navigation__link" href={href}>
-              <i className={`${icon} navigation__icon`} />
-              <span className="navigation__span">{text}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className="navigation-wrapper">
+      <i
+        onClick={handleNavState}
+        className={`fa-solid fa-bars navigation__bars ${
+          isNavbarOpened ? "hidden" : ""
+        }`}
+      ></i>
+      <nav
+        className={`navigation ${isNavbarOpened ? "overlay" : ""}`}
+        onClick={handleNavState}
+      >
+        <i
+          onClick={handleNavState}
+          className={`fa-solid fa-xmark navigation__bars ${
+            isNavbarOpened ? "" : "hidden"
+          }`}
+        ></i>
+        <ul className="navigation__list">
+          {linksList.map(({ text, icon, href, key }) => (
+            <li className="navigation__item" key={key}>
+              <a className="navigation__link" href={href}>
+                <i className={`${icon} navigation__icon`} />
+                <span className="navigation__span">{text}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
