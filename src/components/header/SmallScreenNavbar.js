eeb59b0a21f33439f4219157b29f402e8089db11
Debora Bucci <dbucci@protonmail.com>
2023-03-30 21:15:18 -0300

    refactor code in the SmallScreenNavbar component

--- src/components/header/SmallScreenNavbar.js
+++ src/components/header/SmallScreenNavbar.js
@@ -8,39 +8,40 @@ const SmallScreenNavbar = ({
   const headerHandler = () =>
     isWindowSmall && setIsNavbarOpened((prev) => !prev);
 
+  const btn = (
+    <div
+      className={`header__nav--bars ${
+        isNavbarOpened ? "header__nav--hidden" : "header__nav--hidden"
+      }`}
+      onClick={headerHandler}
+    >
+      <i
+        className={`fa-solid ${!isNavbarOpened ? "fa-bars" : "fa-xmark"} `}
+      ></i>
+    </div>
+  );
+
   const tinyNavbar = (
     <nav className="header__sidebar-wrapper">
-      <i
-        onClick={headerHandler}
-        className={`fa-solid fa-bars header__nav--bars ${
-          isNavbarOpened ? "header__nav--hidden" : ""
-        }`}
-      ></i>
-      <ul
-        className={`header__icon-list ${
-          !isNavbarOpened ? "header__icon-list--overlay" : ""
-        }`}
-      >
-        {linksList.map(({ icon, href, key }) => (
-          <li key={key} onClick={linkClickHandler}>
-            <a href={href}>
-              <i className={`${icon} header__nav--icon`} />
-            </a>
-          </li>
-        ))}
-      </ul>
+      {btn}
+      {isWindowSmall && !isNavbarOpened && (
+        <ul
+          className={`header__icon-list ${
+            !isNavbarOpened ? "header__icon-list--overlay" : ""
+          }`}
+        >
+          {linksList.map(({ icon, href, key }) => (
+            <li key={key} onClick={linkClickHandler}>
+              <a href={href}>
+                <i className={`${icon} header__nav--icon`} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 
-  const closeBtn = (
-    <i
-      onClick={headerHandler}
-      className={`fa-solid fa-xmark header__nav--bars ${
-        isNavbarOpened ? "" : "header__nav--hidden"
-      }`}
-    ></i>
-  );
-
-  return isWindowSmall && !isNavbarOpened ? tinyNavbar : closeBtn;
+  return tinyNavbar;
 };
 export default SmallScreenNavbar;
