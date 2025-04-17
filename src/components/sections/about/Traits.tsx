b048750dbb0492b1b11be52014e77a2b1fde0a35
debora <dbucci@protonmail.com>
2025-04-17 16:45:22 -0300

    remove title

--- src/components/sections/about/Traits.tsx
+++ src/components/sections/about/Traits.tsx
@@ -8,9 +8,6 @@ const Traits = () => {
 
   return (
     <div className="traits" data-aos="fade-up">
-      <h3 className="heading-tertiary u-margin-bottom-medium">
-        {about.titles.traits[language]}
-      </h3>
       <ul className="traits__list">
         {traits.map(({ title, content, icon }, i) => (
           <li className="traits__card" key={Math.random()}>
