aa32de14848b35bf6d5a0384655b17073979b9af
debora <dbucci@protonmail.com>
2025-04-17 15:58:16 -0300

    create a navbar for the projects, and style it using styled components (comment it for later use), and use the newly created projects array for passing the information to the ProjectCard

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -2,31 +2,42 @@ import { useContext } from "react";
 
 import Header from "../../UI/Header";
 import Link from "../../UI/Link";
-import Project from "./Project";
+import ProjectCard from "./ProjectCard";
 
 import LanguageContext from "../../../context/language-context";
-import { projects } from "../../../database/languages-text";
+import styled from "styled-components";
+import { projects } from "../../../database/projects";
+import { projects as prjs } from "../../../database/languages-text";
 
 const Projects = () => {
   const { language } = useContext(LanguageContext);
-  const options = projects.opts[language];
+  const options = prjs.opts[language];
 
   return (
     <section className="section-projects" id="projects">
       <Header section="projects" className="projects-heading" />
 
-      <div className="section-projects__list" data-aos="fade-up">
-        {projects.arr.map((project) => {
+      {/* <ProjectNavbar data-aos="fade-up">
+        <li className="selected">All</li>
+
+        <li>Web</li>
+
+        <li>Mobile</li>
+
+        <li>Other</li>
+      </ProjectNavbar> */}
+
+      <ProjectList className="section-projects__list" data-aos="fade-up">
+        {projects.map((project) => {
           return (
-            <Project
-              key={`project ${project.num}`}
+            <ProjectCard
+              key={`project ${project.title}`}
               project={project}
-              opts={options}
               language={language}
             />
           );
         })}
-      </div>
+      </ProjectList>
 
       <div
         className="u-margin-top-medium section-projects__cta"
@@ -44,3 +55,37 @@ const Projects = () => {
 };
 
 export default Projects;
+
+const ProjectList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 3rem;
+  max-width: 120rem;
+`;
+
+const ProjectNavbar = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 3rem;
+  padding: 0 2rem;
+  margin: 3rem 0 2rem 0;
+  border-radius: 1.2rem;
+  font-size: 1.6rem;
+  color: white;
+  background-color: #2b2b2b;
+
+  li {
+    cursor: pointer;
+    height: 100%;
+    padding: 1rem 2rem;
+    transition: all 0.2s;
+
+    &.selected {
+      background-color: #d83cff;
+    }
+  }
+
+  li:not(.selected):hover {
+    color: #d83cff;
+  }
+`;
